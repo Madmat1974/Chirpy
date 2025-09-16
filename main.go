9b06a91ad32fb51e721f4910f439cbd98aa7a54a
Martin <MartinNiswonger@gmail.com>
2025-09-16 08:40:04 -0500

    ch1 l11

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 )
 
 func main() {
+
+	const filepathRoot = "."
+
 	mux := http.NewServeMux()
 
 	server := &http.Server{
@@ -17,8 +20,12 @@ func main() {
 		IdleTimeout:  15 * time.Second,
 	}
 
-	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/", fs)
+	fs := http.FileServer(http.Dir(filepathRoot))
+	mux.HandleFunc("/healthz", healthHandler)
+	mux.Handle("/app/", http.StripPrefix("/app", fs))
+	mux.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/app/", http.StatusMovedPermanently)
+	})
 
 	fmt.Printf("Starting server on %s", server.Addr)
 	err := server.ListenAndServe()
@@ -26,4 +33,10 @@ func main() {
 		fmt.Printf("Server failed to start: %v", err)
 	}
 
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
